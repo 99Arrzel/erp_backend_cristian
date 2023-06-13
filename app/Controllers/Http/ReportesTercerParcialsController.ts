7ef664ab3eaf0852f8125de6b13d12e58b41e3fe
Andres Carrillo <af.carrillo@live.com>
2023-06-13 14:04:47 -0400

    Test

--- app/Controllers/Http/ReportesTercerParcialsController.ts
+++ app/Controllers/Http/ReportesTercerParcialsController.ts
@@ -271,7 +271,7 @@ export default class ReportesTercerParcialsController {
     }
     console.log("===================");
 
-    console.log(cuentas_con_sumas);
+    console.log(cuentas_con_sumas.map((cuenta) => cuenta.codigo).join(","));
 
     //Ahora si, armamos el balance
     const cuentas_detalles = cuentas_con_sumas.map((cuenta) => {

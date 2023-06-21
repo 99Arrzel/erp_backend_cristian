84a6afcee50024d074fed15016ca1659e84b18d7
Andres Carrillo <af.carrillo@live.com>
2023-06-20 20:15:59 -0400

    modified:   app/Controllers/Http/ArticulosController.ts

--- app/Controllers/Http/ArticulosController.ts
+++ app/Controllers/Http/ArticulosController.ts
@@ -1,5 +1,4 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import Articulo from 'App/Models/Articulo';
 import Categoria from 'App/Models/Categoria';
 import Empresa from 'App/Models/Empresa';
 
